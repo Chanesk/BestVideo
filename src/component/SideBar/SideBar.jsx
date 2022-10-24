bc5d8c1db8a4986a391d3699fd564c100ac46f0f
Chanesk <chanesk01@gmail.com>
2022-10-24 10:52:02 +0100

    message pour ignorer le fichier d'envirronement

--- src/component/SideBar/SideBar.jsx
+++ src/component/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SideBarRow from '../SideBarRow/SideBarRow';
+import SubscriptionIcon from '@material-ui/icons/Subscriptions';
 import VideoSubcription from '../VideoSubcription/VideoSubcription';
 import './SideBar.css';
 import HomeIcon from '@material-ui/icons/Home';
@@ -9,6 +10,8 @@ const SideBar = () =>{
         <>
                 <div className='sidebar'>
                     <SideBarRow Icon={HomeIcon} title='Home' />
+                    <SideBarRow Icon={SubscriptionIcon} title ='Subscription' />
+                    <h2></h2>
                     <VideoSubcription />
                 </div>
            
